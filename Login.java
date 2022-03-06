0c73fa8a717dc70b3e5c8ffd6c3d8391c6363039
YuboChen20 <100442328+YuboChen20@users.noreply.github.com>
2022-03-06 17:50:56 +0100

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,22 +1,22 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JTextArea;
+import javax.swing.JEditorPane;
 
 public class Login extends JFrame {
 
-	private JFrame frame;
-	private JPasswordField passwordField;
+	private JPanel contentPane;
 	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -25,8 +25,8 @@ public class Login extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login window = new Login();
-					window.frame.setVisible(true);
+					Login frame = new Login();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,52 +35,44 @@ public class Login extends JFrame {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public Login() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Usuario :");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel.setBounds(32, 36, 106, 21);
-		frame.getContentPane().add(lblNewLabel);
-		
-		passwordField = new JPasswordField();
-		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		passwordField.setBounds(178, 92, 209, 33);
-		frame.getContentPane().add(passwordField);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setBounds(29, 34, 99, 26);
+		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Contrase\u00F1a :");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1.setBounds(31, 99, 107, 16);
-		frame.getContentPane().add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(29, 89, 106, 13);
+		contentPane.add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(178, 33, 209, 33);
-		frame.getContentPane().add(textField);
+		textField.setBounds(162, 37, 229, 25);
+		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Acceder a la cuenta");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(107, 137, 209, 39);
-		frame.getContentPane().add(btnNewButton);
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(162, 81, 229, 26);
+		contentPane.add(textField_1);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(32, 199, 355, 39);
-		frame.getContentPane().add(textArea);
+		JButton btnNewButton = new JButton("Acceder a la cuenta");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton.setBounds(103, 134, 229, 38);
+		contentPane.add(btnNewButton);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBounds(29, 198, 362, 43);
+		contentPane.add(editorPane);
 	}
+
 }
